98f5626fca25ac5cd3c81cc646cf7bb102617ff5
contessa <tessa.fallon@gmail.com>
2013-12-01 15:31:05 -0500

    Add music hash with artist, song, genre values

--- app.rb
+++ app.rb
@@ -1,11 +1,31 @@
-require './class_genre'
-require './class_song'
-require './class_artist'
-#for part B
-#ile_names = Dir.entries("data").select {|f| !File.directory? f}
+require './lib/class_artist'
+require './lib/class_song'
+require './lib/class_genre'
 
-#puts file_names.map {|string| string.split!(" - ")}
+my_dir = Dir.new("data")
 
-#list artists with songs
-artist1 = Artist.new
-artist1.songs
+entries = Dir.entries("data").select {|f| !File.directory? f}
+
+first_split = [] #artist eo line
+second_split = [] #title and genre
+genres = []
+entries.each {|parts| first_split << parts.split(" - ")}  #REFACTOR gross
+first_split.each {|genre| second_split << genre[1].split(' [')}
+second_split.each {|genre| genre[1].slice!('].mp3')}
+first_split.each {|junk| junk.delete_at(1)}
+second_split.each {|stuff| genres << stuff.pop}
+
+music_hash = Hash.new
+
+puts music_hash[:artists] = [:artist, :song, :genre] #add values inrecemeting by index number
+
+i = 0
+            (0..100).each do |i|
+              music_hash[:artists][i] = [:artist => first_split[i], :song => second_split[i], :genre => genres[i]]
+            end
+
+
+puts music_hash.inspect
+
+
+#if array index is the same, put to new array
